dd1d92eb6f2701f3aba851e6d60982f0b24747c7
CarolSum <905965287@qq.com>
2018-12-26 00:27:16 +0800

    after migrate --reset, Delpoy a new Contest, and find out it WORK

--- src/front-end/src/components/Contest.vue
+++ src/front-end/src/components/Contest.vue
@@ -119,7 +119,7 @@ export default {
 		}
 	},
 	mounted () {
-		this.$contracts.Contest.at("0xb2dd0baa6ec9f494d8bce14bf2030dc5d86fee98").then(function(instance){
+		this.$contracts.Contest.at("0x7317fa42e85a36f8411a459b34e70c95806b8d6b").then(function(instance){
 			console.log(instance);
 			return instance.getContestBaseInfo.call();
 		}).then(function(balance){
