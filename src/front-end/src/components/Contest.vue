8891e1a37c6ae8f1f918e90d6f6effb6001d470b
CarolSum <905965287@qq.com>
2018-12-27 21:17:10 +0800

    添加显示解答

--- src/front-end/src/components/Contest.vue
+++ src/front-end/src/components/Contest.vue
@@ -62,7 +62,7 @@ import {
 } from 'ant-design-vue';
 import IconText from './share/IconText';
 import ProblemCard from './share/ProblemCard';
-import { baseInfoWrapper } from '../utils/ObjectWrapper';
+import { baseInfoWrapper, allInfoWrapper } from '../utils/ObjectWrapper';
 import { notification } from 'ant-design-vue';
 import { SET_BASEINFO } from '../store/mutations';
 
@@ -81,39 +81,15 @@ export default {
 	},
 	data: function () {
 		return {
-			contest: {
-				contestid: 111,
-				// 以下是基本信息
-				desc: '这是描述信息这是描述信息这是描述信息这是描述信息这是描述信息这是描述信息',
-				isFinished: false,
-				title: '孔Bob举办的第一场pk赛',
-				cost: 10,
-				bonus: 1000,
-				ddl: '2018-12-20/20:30',
-				sponsor: '0x12321312313',
-				// 题目内容
-				content: '[{"id":0,"title":"大气中的水汽主要来源于","choice":["太阳","植物蒸腾","地球表面水体蒸发","大气本身"]},{"id":1,"title":"世界上各月降水量都很多的地区在：","choice":["南北纬300—400大陆东岸","南北回归线附近","南北纬400—600大陆东岸","赤道附近"]},{"id":2,"title":"我国庐山成为避暑胜地的主要因素是","choice":["纬度因素","海陆因素","洋流因素","地形因素"]}]',
-				// 答案
-				answer: '{"0":2,"1":0,"2":3}',
-				// 赢家
-				winner: '0x12312312312312312312'
-			},
 			baseContestInfo: {},
-			getContestAllInfo: {},
+			allContestInfo: {},
 			user_submit: {},
 			standard_answer: {},
 			content: [],
 			isSubmitted: false,
+			isFinished: false,
 			isParticipator: true
 		}
-	},
-	computed: {
-		resolveContent: function () {
-			return JSON.parse(this.baseContestInfo.content);
-		},
-		resolveAnswer: function () {
-			return JSON.parse(this.baseContestInfo.answer);
-		},
 	},
   methods: {
 		jumpToAnswer () {
@@ -165,7 +141,13 @@ export default {
 				// 获取题目详细信息
 				let allInfo = await instance.getContestAllInfo.call(); 
 				console.log(allInfo);
-				// let res = allInfoWrapper(info);
+				let res = allInfoWrapper(info);
+				console.log(res);
+				// 时间戳转为Date
+				res.ddl = new Date(res.ddl);
+				this.standard_answer = JSON.parse(res.answer.replace(/\'/g,"\""));
+				this.content = JSON.parse(res.content.replace(/\'/g,"\""));
+				this.allContestInfo = res;
 			}
 
 			// 查看用户是否已经参与这次比赛
